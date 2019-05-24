f8049fe684d47bf5118049c5f9de2419eb5cac4c
patrick@enclave.vn <patrick@enclave.vn>
2019-05-24 10:53:30 +0700

    24/May/2019

--- lib/model/topic.dart
+++ lib/model/topic.dart
@@ -14,7 +14,7 @@ final fortniteTopics = [
       answerCount: "1241"),
   Topic(
       question:
-          "Changing background color of archive drop down on product page?",
+          "Changing background color?",
       recentAnswer:
           "Has anyone identified a code that would allow me to change the background color and font color?",
       answerCount: "549")
@@ -27,7 +27,7 @@ final pubgTopics = [
           "Literally done with game until they balance the map better so that the blue corner",
       answerCount: "112"),
   Topic(
-      question: "Code Injection to solve LinkedIn broken image issue",
+      question: "Code Injection to solve",
       recentAnswer:
           "I need to add the following code to my Squarespace site to make the thumbnail image show up",
       answerCount: "445")
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -2,13 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:video_game_app/common/app_background.dart';
 import 'package:video_game_app/common/label_value_widget.dart';
 import 'package:video_game_app/model/forum.dart';
+import 'package:video_game_app/model/topic.dart';
 import 'package:video_game_app/styleguide/colors.dart';
 import 'package:video_game_app/styleguide/text_styles.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final Forum forum;
 
   DetailsPage({this.forum});
+
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _fadeAnimation;
+  Animation<double> _scaleAnimation;
+  Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(_controller);
+    _offsetAnimation = Tween<Offset>(begin: Offset(0, 1), end: Offset(0, 0))
+        .animate(_controller);
+  }
+
+  _playAnimation() {
+    _controller.reset();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +63,7 @@ class DetailsPage extends StatelessWidget {
                 height: 60.0,
               ),
               InkWell(
-                onTap: (){
+                onTap: () {
                   Navigator.pop(context);
                 },
                 child: Padding(
@@ -41,38 +77,49 @@ class DetailsPage extends StatelessWidget {
               SizedBox(
                 height: 30.0,
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 100.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    LabelValueWidget(
-                      value: forum.topics.length.toString(),
-                      label: "topics",
-                      labelStyle: whiteLabelTextStyle,
-                      valueStyle: whiteValueTextStyle,
+              FutureBuilder(
+                future: _playAnimation(),
+                builder: (context, snapshot) {
+                  return FadeTransition(
+                    opacity: _fadeAnimation,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 100.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          LabelValueWidget(
+                            value: widget.forum.topics.length.toString(),
+                            label: "topics",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          ),
+                          LabelValueWidget(
+                            value: widget.forum.threads,
+                            label: "threads",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          ),
+                          LabelValueWidget(
+                            value: widget.forum.subs,
+                            label: "subs",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          ),
+                        ],
+                      ),
                     ),
-                    LabelValueWidget(
-                      value: forum.threads,
-                      label: "threads",
-                      labelStyle: whiteLabelTextStyle,
-                      valueStyle: whiteValueTextStyle,
-                    ),
-                    LabelValueWidget(
-                      value: forum.subs,
-                      label: "subs",
-                      labelStyle: whiteLabelTextStyle,
-                      valueStyle: whiteValueTextStyle,
-                    ),
-                  ],
-                ),
+                  );
+                },
               ),
               SizedBox(height: 30.0),
-              ClipRRect(
-                  borderRadius:
-                      BorderRadius.only(topLeft: Radius.circular(60.0)),
-                  child: Image.asset(forum.imagePath)),
+              Hero(
+                tag: widget.forum.title,
+                child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(60.0)),
+                    child: Image.asset(widget.forum.imagePath)),
+              ),
             ],
           ),
           Align(
@@ -82,22 +129,108 @@ class DetailsPage extends StatelessWidget {
               child: Container(
                 height: 250.0,
                 color: Colors.white,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Topics",
+                        style: subHeadingStyle.copyWith(fontSize: 26),
+                      ),
+                      Expanded(
+                        child: NotificationListener<
+                            OverscrollIndicatorNotification>(
+                          onNotification: (overscroll) {
+                            overscroll.disallowGlow();
+                          },
+                          child: SlideTransition(
+                            position: _offsetAnimation,
+                            child: Column(
+//                            shrinkWrap: true,
+                              children: <Widget>[
+                                TopicsTile(topic: widget.forum.topics[0]),
+                                TopicsTile(topic: widget.forum.topics[1])
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
           Positioned(
             bottom: 210.0,
             right: 30.0,
-            child: Material(
-              elevation: 10.0,
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text(forum.rank, style: rankBigStyle,)
+            child: ScaleTransition(
+              scale: _scaleAnimation,
+              child: Material(
+                elevation: 10.0,
+                child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Text(
+                      widget.forum.rank,
+                      style: rankBigStyle,
+                    )),
+                color: Colors.white,
+                shape: CircleBorder(),
               ),
-              color: Colors.white,
-              shape: CircleBorder(),
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class TopicsTile extends StatelessWidget {
+  final Topic topic;
+
+  TopicsTile({this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                topic.question,
+                style: topicQuestionTextStyle,
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    color: primaryColor),
+                child: Text(
+                  topic.answerCount,
+                  style: topicAnswerCountTextStyle,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              topic.recentAnswer,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: topicAnswerTextStyle,
+            ),
+          )
         ],
       ),
     );
